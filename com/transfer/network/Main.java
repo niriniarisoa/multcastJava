776e2f42d97f1681e39e6e74853555d8803fb3e5
niriniarisoa <muriellaniriniarisoa@gmail.com>
2024-10-17 17:21:15 +0200

    send et receive mandeha

--- com/transfer/network/Main.java
+++ com/transfer/network/Main.java
@@ -1,18 +1,32 @@
 package com.transfer.network;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String multicastAddress = "230.0.0.0"; // Adresse multicast
         int port = 4446;
-        String filePath = "E:\\projet\\progRes\\text.txt"; // Chemin vers le fichier à envoyer
 
-        MulticastSender sender = new MulticastSender(multicastAddress, port);
+        // Lancer le receiver dans un thread séparé
         MulticastReceiver receiver = new MulticastReceiver(multicastAddress, port);
-
-        // Lancer le réceptionnaire dans un nouveau thread
         new Thread(receiver::startReception).start();
 
-        // Envoyer le fichier spécifié
-        sender.sendFile(filePath);
+        // Demander à l'utilisateur s'il veut envoyer un fichier
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Voulez-vous envoyer un fichier ? (oui/non)");
+        String response = scanner.nextLine();
+
+        if (response.equalsIgnoreCase("oui")) {
+            // Chemin vers le fichier à envoyer
+            System.out.println("Entrez le chemin complet du fichier à envoyer : ");
+            String filePath = scanner.nextLine();
+
+            MulticastSender sender = new MulticastSender(multicastAddress, port);
+            
+            // Lancer l'envoi du fichier dans un autre thread
+            new Thread(() -> sender.sendFile(filePath)).start();
+        }
+
+        scanner.close();
     }
 }
